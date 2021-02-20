cc330e5ceed006833fbd1dff8c3542db48290919
christianngouw <christiangouw168@gmail.com>
2021-02-20 22:05:52 +0700

    fixing MobileNavigation component styling

--- components/MobileNavigation.js
+++ components/MobileNavigation.js
@@ -1,7 +1,7 @@
 const MobileNavigation = Vue.component('MobileNavigationn' , {
     template : `
         <div class='pt-6 flex justify-center'>
-            <div class='rounded text-gray-300 bg-gray-300 cursor-pointer h-1 w-20 select-none '> - </div>
+            <div class='rounded text-gray-700 bg-gray-700 cursor-pointer h-1 w-32 select-none '> - </div>
         </div>
     `
 })
