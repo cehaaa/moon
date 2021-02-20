484527309e89b47b26ecbad1309f14fdce11afa0
christianngouw <christiangouw168@gmail.com>
2021-02-20 23:12:30 +0700

    code refactor

--- components/MobileNavigation.js
+++ components/MobileNavigation.js
@@ -1,7 +1,7 @@
 const MobileNavigation = Vue.component('MobileNavigationn' , {
     template : `
-        <div class='mt-6 flex justify-center absolute bottom-0 right-1/3'>
-            <div class='rounded text-gray-700 bg-gray-700 cursor-pointer h-1 w-32 select-none '> </div>
+        <div class='pt-6 flex justify-center'>
+            <div class='rounded text-gray-700 bg-gray-700 cursor-pointer h-1 w-32 select-none '>  </div>
         </div>
     `
 })
